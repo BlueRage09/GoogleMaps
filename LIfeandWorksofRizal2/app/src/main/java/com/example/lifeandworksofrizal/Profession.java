7091af32831947b1cea2f672d7370b5020aa4e2c
Bluerage09 <tacdolrey50@gmail.com>
2021-01-31 19:15:10 +0800

    FinalProject

---
+++ LIfeandWorksofRizal2/app/src/main/java/com/example/lifeandworksofrizal/Profession.java
@@ -0,0 +1,14 @@
+package com.example.lifeandworksofrizal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profession extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profession);
+    }
+}
